1d15f611f56127cb8b7de1f28ca48c68860c15d7
Shovkat1987 <122811814+Shovkat1987@users.noreply.github.com>
2023-02-26 13:10:01 +0500

    Add files via upload

---
+++ Alisher/js/index.js
@@ -0,0 +1,22 @@
+
+
+const a = 500;
+const b = 250;
+const h = 120;
+const v = 11000;
+const c = 12000;
+const s = (a * v) + (b * v) + (h * c);
+
+
+
+const d = s;
+
+
+const total = +prompt('soqqani kiritin')
+
+if (total > d) {
+    console.log('Oq yol Alisher');
+} else {
+    console.log('Alisher, ozgina sabr qilish kerak bo’lar ekan.');
+}
+
